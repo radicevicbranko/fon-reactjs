c5d800d56b5759e3f47eabd529da1e7e205c80d3
Branko Radicevic <branko@getpenta.com>
2018-05-07 10:00:48 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    const element = <div className="App">Hello, world!</div>;
+    return element;
+  }
+}
+
+export default App;
