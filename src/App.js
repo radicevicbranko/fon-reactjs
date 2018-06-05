98989dcf64e94c4e4fa9f27c725d501ffd687e01
Branko Radicevic <branko@getpenta.com>
2018-06-05 10:43:21 +0200

    Add top navigation

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Data from "./books.json";
 import BookList from "./components/books-list";
 import BookDetails from "./components/book-details";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Navbar, FormGroup, FormControl, Button } from "react-bootstrap";
 
 class App extends Component {
   constructor() {
@@ -13,9 +14,43 @@ class App extends Component {
     };
   }
   render() {
-    return <div className="App">{this.showContent()}</div>;
+    return (
+      <div className="App">
+        {this.showNavbar()}
+        {this.showContent()}
+      </div>
+    );
   }
 
+  showNavbar = () => {
+    return (
+      <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#home">My first awesome App</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Navbar.Form pullLeft>
+            <FormGroup>
+              <FormControl type="text" placeholder="Search" />
+            </FormGroup>{" "}
+            <Button type="submit">Search</Button>
+          </Navbar.Form>
+          <Navbar.Text>
+            Hello user: <Navbar.Link href="#">Mark Otto</Navbar.Link>
+          </Navbar.Text>
+          <Navbar.Text pullRight>
+            <span className="icon-wrapper">
+              <i className="fa fa-shopping-cart" aria-hidden="true" />
+              <span className="badge">2</span>
+            </span>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  };
   showContent = () => {
     return (
       <Router>
