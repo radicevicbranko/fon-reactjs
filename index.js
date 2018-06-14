a85dbcd46d101c12581119ba532a67ec69819dd6
Branko Radicevic <branko@getpenta.com>
2018-06-14 19:44:05 +0200

    Fix deploy

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ var exampleProxy = proxy(options);
 app.use("/api", exampleProxy);
 app.use(express.static(path.join(__dirname, "build")));
 
-app.post('/place-order', (req, res) => res.send('Ok!'))
+app.post("/place-order", (req, res) => res.send("Ok!"));
 
 app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(9000);
+app.listen(process.env.PORT || 9000);
